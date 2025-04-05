e32aabcd4b77945278b3b48b2bfb7cb25c98b15f
Claudia <claudia@michaels-air.lan>
2025-04-05 14:50:21 -0400

    initial form UI setup

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+const selected = ref('')
+
+</script>
+
+<template>
+  <h1>What Should I Wear Today?</h1>
+  <h3 style="margin-top: 2rem;">Zipcode: <input type="text" required></h3>
+  <h3 style="margin-top: 2rem;">How do cats wear pants? {{ selected }}</h3>
+  <div style="margin-top: 1rem;">
+    <input type="radio" id="one" value="On two legs" v-model="selected" />
+    <label style="padding-left: .5rem;" for="one">On two legs</label>
+  </div>
+  <div style="margin-top: 1rem;">
+    <input type="radio" id="two" value="On four legs" v-model="selected" />
+    <label style="padding-left: .5rem;" for="two">On four legs</label>
+  </div>
+  <button style="margin-top: 2rem; padding: 1rem; border-radius: 30px; border:0px; background:#42b883; color: #ffffff;" type="submit">Generate Today's Outfit</button>
+</template>
+
+
+
+<style scoped>
+</style>
